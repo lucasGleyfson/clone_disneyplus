b022a9867091edd094b8c0ee34a405a2cb1f16ba
lucas barros <lucasgleyfson59@gmail.com>
2025-02-25 11:36:44 -0300

    criando a seção de planos

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const imagemin = require('gulp-imagemin');
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
-        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(sass({outputStyle: "compressed"}))
         .pipe(gulp.dest('./dist/css'));
 }
 
